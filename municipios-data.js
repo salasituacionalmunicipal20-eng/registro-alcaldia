bb53aeaa66efd584d3a7d98c2557139b9d396273
salasituacionalmunicipal20-eng <salasituacionalmunicipal20@gmail.com>
2026-06-28 02:15:21 +0200

    Refugiados: edad junto a fecha, cascada estado-municipio, atencion medica, campo centro medico, quitar opcion responsable-UBCH y panel de centro electoral

---
+++ municipios-data.js
@@ -0,0 +1,29 @@
+// municipios-data.js — Municipios por estado de Venezuela (parroquias/localidades para
+// La Guaira y Distrito Capital, que son un solo municipio). Define window.MUNICIPIOS.
+// Las claves coinciden EXACTAMENTE con las etiquetas de ESTADOS en refugiados.html.
+window.MUNICIPIOS = {
+  "La Guaira (Vargas)": ["Catia La Mar","Carlos Soublette","Urimare","Maiquetía","La Guaira","Macuto","Caraballeda","Naiguatá","Caruao","Carayaca","El Junko"],
+  "Distrito Capital": ["Altagracia","Antímano","Caricuao","Catedral","Coche","El Junquito","El Paraíso","El Recreo","El Valle","La Candelaria","La Pastora","La Vega","Macarao","San Agustín","San Bernardino","San José","San Juan","San Pedro","Santa Rosalía","Santa Teresa","Sucre","23 de Enero"],
+  "Miranda": ["Acevedo","Andrés Bello","Baruta","Brión","Buroz","Carrizal","Chacao","Cristóbal Rojas","El Hatillo","Guaicaipuro","Independencia","Lander","Los Salias","Páez","Paz Castillo","Pedro Gual","Plaza","Simón Bolívar","Sucre","Urdaneta","Zamora"],
+  "Aragua": ["Bolívar","Camatagua","Francisco Linares Alcántara","Girardot","José Ángel Lamas","José Félix Ribas","José Rafael Revenga","Libertador","Mario Briceño Iragorry","Ocumare de la Costa de Oro","San Casimiro","San Sebastián","Santiago Mariño","Santos Michelena","Sucre","Tovar","Urdaneta","Zamora"],
+  "Carabobo": ["Bejuma","Carlos Arvelo","Diego Ibarra","Guacara","Juan José Mora","Libertador","Los Guayos","Miranda","Montalbán","Naguanagua","Puerto Cabello","San Diego","San Joaquín","Valencia"],
+  "Anzoátegui": ["Anaco","Aragua","Manuel Ezequiel Bruzual","Diego Bautista Urbaneja","Fernando de Peñalver","Francisco del Carmen Carvajal","Francisco de Miranda","Guanta","Independencia","José Gregorio Monagas","Juan Antonio Sotillo","Juan Manuel Cajigal","Libertad","Pedro María Freites","Píritu","San José de Guanipa","San Juan de Capistrano","Santa Ana","Simón Bolívar","Simón Rodríguez","Sir Arthur McGregor"],
+  "Amazonas": ["Alto Orinoco","Atabapo","Atures","Autana","Manapiare","Maroa","Río Negro"],
+  "Apure": ["Achaguas","Biruaca","Muñoz","Páez","Pedro Camejo","Rómulo Gallegos","San Fernando"],
+  "Barinas": ["Alberto Arvelo Torrealba","Andrés Eloy Blanco","Antonio José de Sucre","Arismendi","Barinas","Bolívar","Cruz Paredes","Ezequiel Zamora","Obispos","Pedraza","Rojas","Sosa"],
+  "Bolívar": ["Angostura (Raúl Leoni)","Caroní","Cedeño","El Callao","Gran Sabana","Heres","Padre Pedro Chien","Piar","Roscio","Sifontes","Sucre"],
+  "Cojedes": ["Anzoátegui","Tinaco","Girardot","Lima Blanco","Pao de San Juan Bautista","Ricaurte","Rómulo Gallegos","San Carlos","Tinaquillo"],
+  "Delta Amacuro": ["Antonio Díaz","Casacoima","Pedernales","Tucupita"],
+  "Falcón": ["Acosta","Bolívar","Buchivacoa","Cacique Manaure","Carirubana","Colina","Dabajuro","Democracia","Falcón","Federación","Jacura","José Laurencio Silva","Los Taques","Mauroa","Miranda","Monseñor Iturriza","Palmasola","Petit","Píritu","San Francisco","Sucre","Tocópero","Unión","Urumaco","Zamora"],
+  "Guárico": ["Camaguán","Chaguaramas","El Socorro","Francisco de Miranda","José Félix Ribas","José Tadeo Monagas","Juan Germán Roscio","Juan José Rondón","Julián Mellado","Leonardo Infante","Ortiz","Pedro Zaraza","San Gerónimo de Guayabal","San José de Guaribe","Santa María de Ipire"],
+  "Lara": ["Andrés Eloy Blanco","Crespo","Iribarren","Jiménez","Morán","Palavecino","Simón Planas","Torres","Urdaneta"],
+  "Mérida": ["Alberto Adriani","Andrés Bello","Antonio Pinto Salinas","Aricagua","Arzobispo Chacón","Campo Elías","Caracciolo Parra Olmedo","Cardenal Quintero","Guaraque","Julio César Salas","Justo Briceño","Libertador","Miranda","Obispo Ramos de Lora","Padre Noguera","Pueblo Llano","Rangel","Rivas Dávila","Santos Marquina","Sucre","Tovar","Tulio Febres Cordero","Zea"],
+  "Monagas": ["Acosta","Aguasay","Bolívar","Caripe","Cedeño","Ezequiel Zamora","Libertador","Maturín","Piar","Punceres","Santa Bárbara","Sotillo","Uracoa"],
+  "Nueva Esparta": ["Antolín del Campo","Arismendi","Díaz","García","Gómez","Maneiro","Marcano","Mariño","Península de Macanao","Tubores","Villalba"],
+  "Portuguesa": ["Agua Blanca","Araure","Esteller","Guanare","Guanarito","Monseñor José Vicente de Unda","Ospino","Páez","Papelón","San Genaro de Boconoíto","San Rafael de Onoto","Santa Rosalía","Sucre","Turén"],
+  "Sucre": ["Andrés Eloy Blanco","Andrés Mata","Arismendi","Benítez","Bermúdez","Bolívar","Cajigal","Cruz Salmerón Acosta","Libertador","Mariño","Mejía","Montes","Ribero","Sucre","Valdez"],
+  "Táchira": ["Andrés Bello","Antonio Rómulo Costa","Ayacucho","Bolívar","Cárdenas","Córdoba","Fernández Feo","Francisco de Miranda","García de Hevia","Guásimos","Independencia","Jáuregui","José María Vargas","Junín","Libertad","Libertador","Lobatera","Michelena","Panamericano","Pedro María Ureña","Rafael Urdaneta","Samuel Darío Maldonado","San Cristóbal","San Judas Tadeo","Seboruco","Simón Rodríguez","Sucre","Torbes","Uribante"],
+  "Trujillo": ["Andrés Bello","Boconó","Bolívar","Candelaria","Carache","Carvajal","Escuque","José Felipe Márquez Cañizales","Juan Vicente Campo Elías","La Ceiba","Miranda","Monte Carmelo","Motatán","Pampán","Pampanito","Rafael Rangel","San Rafael de Carvajal","Sucre","Trujillo","Urdaneta","Valera"],
+  "Yaracuy": ["Arístides Bastidas","Bolívar","Bruzual","Cocorote","Independencia","José Antonio Páez","La Trinidad","Manuel Monge","Nirgua","Peña","San Felipe","Sucre","Urachiche","Veroes"],
+  "Zulia": ["Almirante Padilla","Baralt","Cabimas","Catatumbo","Colón","Francisco Javier Pulgar","Jesús Enrique Lossada","Jesús María Semprún","La Cañada de Urdaneta","Lagunillas","Machiques de Perijá","Mara","Maracaibo","Miranda","Páez","Rosario de Perijá","San Francisco","Santa Rita","Simón Bolívar","Sucre","Valmore Rodríguez"]
+};
